1a0341e56aaef31b043ca53d67e332225f29fff9
GooseTeaRice <gusti.fatu@gmail.com>
2025-07-14 12:05:52 +1000

    hello

--- src/components/portfolio/Projects.tsx
+++ src/components/portfolio/Projects.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import { motion, useAnimation, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import Background from "../Background";
 
 interface Project {
   id: string;
